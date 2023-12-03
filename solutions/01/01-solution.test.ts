5c29e14427433155d0d7e0e5cc56fbb8e0f16de8
Antonin Vogt <antoninvogt@users.noreply.github.com>
2023-12-03 17:20:54 +0100

    add solution for day 01, part 2

--- solutions/01/01-solution.test.ts
+++ solutions/01/01-solution.test.ts
@@ -1,55 +1,134 @@
 import {
-	readDigitsFromLine,
+	readPureDigitsFromLine,
+	readAllDigitsFromLine,
 	calculateResultSum
 } from "./01-solution";
 
 describe("Day 01", () => {
 	describe("readDigitsFromLine", () => {
-		const puzzleInputLines = [
-			"1abc2",
-			"pqr3stu8vwx",
-			"a1b2c3d4e5f",
-			"treb7uche"
-		];
+		describe("readPureDigitsFromLine", () => {
+			const puzzleInputLines = [
+				"1abc2",
+				"pqr3stu8vwx",
+				"a1b2c3d4e5f",
+				"treb7uche"
+			];
 
-		it("should read the appropriate numbers from puzzle example no. 1", () => {
-			const input = puzzleInputLines[0];
+			it("should read the appropriate numbers from line 1", () => {
+				const input = puzzleInputLines[0];
 
-			const result = readDigitsFromLine(input);
+				const result = readPureDigitsFromLine(input);
 
-			expect(result).toEqual([ 1, 2 ]);
+				expect(result).toEqual([1, 2]);
+			});
+
+			it("should read the appropriate numbers from line 2", () => {
+				const input = puzzleInputLines[1];
+
+				const result = readPureDigitsFromLine(input);
+
+				expect(result).toEqual([3, 8]);
+			});
+
+			it("should read the appropriate numbers from line 3", () => {
+				const input = puzzleInputLines[2];
+
+				const result = readPureDigitsFromLine(input);
+
+				expect(result).toEqual([1, 5]);
+			});
+
+			it("should read the appropriate numbers from line 4", () => {
+				const input = puzzleInputLines[3];
+
+				const result = readPureDigitsFromLine(input);
+
+				expect(result).toEqual([7, 7]);
+			});
+
+			it("should only account for one digit in each place, even if they are adjacent in the input", () => {
+				const input = "76xkqjzqtwonfour";
+
+				const result = readPureDigitsFromLine(input);
+
+				expect(result).toEqual([ 7, 6 ]);
+			});
 		});
+		
+		describe("readAllDigitsFromLine", () => {
+			const puzzleInputLines = [
+				"two1nine",
+				"eightwothree",
+				"abcone2threexyz",
+				"xtwone3four",
+				"4nineeightseven2",
+				"zoneight234",
+				"7pqrstsixteen"
+			];
 
-		it("should read the appropriate numbers from puzzle example no. 2", () => {
-			const input = puzzleInputLines[1];
+			it("should read the appropriate numbers from line 1", () => {
+				const input = puzzleInputLines[0];
 
-			const result = readDigitsFromLine(input);
+				const result = readAllDigitsFromLine(input);
 
-			expect(result).toEqual([ 3, 8 ]);
-		});
+				expect(result).toEqual([2, 9]);
+			});
 
-		it("should read the appropriate numbers from puzzle example no. 3", () => {
-			const input = puzzleInputLines[2];
+			it("should read the appropriate numbers from line 2", () => {
+				const input = puzzleInputLines[1];
 
-			const result = readDigitsFromLine(input);
+				const result = readAllDigitsFromLine(input);
 
-			expect(result).toEqual([ 1, 5 ]);
-		});
+				expect(result).toEqual([8, 3]);
+			});
 
-		it("should read the appropriate numbers from puzzle example no. 4", () => {
-			const input = puzzleInputLines[3];
+			it("should read the appropriate numbers from line 3", () => {
+				const input = puzzleInputLines[2];
 
-			const result = readDigitsFromLine(input);
+				const result = readAllDigitsFromLine(input);
 
-			expect(result).toEqual([ 7, 7 ]);
-		});
+				expect(result).toEqual([1, 3]);
+			});
 
-		it("should only account for one digit in each place, even if they are adjacent in the input", () => {
-			const input = "76xkqjzqtwonfour";
+			it("should read the appropriate numbers from line 4", () => {
+				const input = puzzleInputLines[3];
 
-			const result = readDigitsFromLine(input);
+				const result = readAllDigitsFromLine(input);
 
-			expect(result).toEqual([ 7, 6 ]);
+				expect(result).toEqual([2, 4]);
+			});
+
+			it("should read the appropriate numbers from line 5", () => {
+				const input = puzzleInputLines[4];
+
+				const result = readAllDigitsFromLine(input);
+
+				expect(result).toEqual([4, 2]);
+			});
+
+			it("should read the appropriate numbers from line 6", () => {
+				const input = puzzleInputLines[5];
+
+				const result = readAllDigitsFromLine(input);
+
+				expect(result).toEqual([1, 4]);
+			});
+
+			it("should read the appropriate numbers from line 7", () => {
+				const input = puzzleInputLines[6];
+
+				const result = readAllDigitsFromLine(input);
+
+				expect(result).toEqual([7, 6]);
+			});
+
+			it("should account for two spelled out digits overlapping", () => {
+				const input = "3one8ncctmbsixeighttwonegb";
+
+				const result = readAllDigitsFromLine(input);
+
+				expect(result).toEqual([3, 1]);
+			});
 		});
 	});
 
--- solutions/01/01-solution.ts
+++ solutions/01/01-solution.ts
@@ -1,13 +1,63 @@
 import * as inputHelper from "../../helpers/input";
 
-function readDigitsFromLine(inputLine: string): [ number, number ] {
+type SpelledOutDigit = "one"|"two"|"three"|"four"|"five"|"six"|"seven"|"eight"|"nine";
+
+const numbersForWords: Record<SpelledOutDigit, number> = {
+	one: 1,
+	two: 2,
+	three: 3,
+	four: 4,
+	five: 5,
+	six: 6,
+	seven: 7,
+	eight: 8,
+	nine: 9
+}
+
+function readPureDigitsFromLine(inputLine: string): [ number, number ] {
 	const firstDigitMatcher = (/^\D*?(\d)/);
 	const secondDigitMatcher = (/(\d)\D*$/);
 
 	const firstDigitMatches = inputLine.match(firstDigitMatcher)!;
 	const secondDigitMatches = inputLine.match(secondDigitMatcher)!;
-	
+
 	return [ Number(firstDigitMatches[1]), Number(secondDigitMatches[1]) ];
+}
+
+function readNumberFromMatch(match: string): number {
+	const parsedInteger = parseInt(match, 10);
+	
+	return (!Number.isNaN(parsedInteger)) ? parsedInteger : numbersForWords[match as SpelledOutDigit]
+}
+
+function captureOverlappingMatches(regExpLiteral: RegExp, testString: string): string[] {
+	const matches = [];
+	let currentMatch;
+	
+	const matcher = new RegExp(regExpLiteral);
+
+	while (currentMatch = matcher.exec(testString)) {
+		const matchedDigit = currentMatch[0]
+		
+		matches.push(matchedDigit);
+		
+		if (matchedDigit.length > 1) {
+			// move index back to one character after the last match to allow overlapping words to be captured (like in "twone", which should capture "two" and then "one")
+			
+			matcher.lastIndex = currentMatch.index + 1;
+		}
+	}
+	
+	return matches;
+}
+
+function readAllDigitsFromLine(inputLine: string): [ number, number ] {
+	const allMatches = captureOverlappingMatches(/(\d|one|two|three|four|five|six|seven|eight|nine)/g, inputLine)!;
+
+	const firstDigit = readNumberFromMatch(allMatches[0]);
+	const secondDigit = readNumberFromMatch(allMatches[allMatches.length - 1]);
+
+	return [ firstDigit, secondDigit ];
 }
 
 function calculateResultSum(digitPairs: [ number, number ][]): number {
@@ -19,16 +69,18 @@ function calculateResultSum(digitPairs: [ number, number ][]): number {
 async function solve(): Promise<string[]> {
 	const puzzleInput = await inputHelper.readPuzzleInput(__dirname, "./01-input.txt");
 
-	const digitPairs = puzzleInput.map(readDigitsFromLine);
-	const resultPartOne = calculateResultSum(digitPairs);
+	const resultPartOne = calculateResultSum(puzzleInput.map(readPureDigitsFromLine));
+	const resultPartTwo = calculateResultSum(puzzleInput.map(readAllDigitsFromLine));
 
 	return [
-		String(resultPartOne)
+		String(resultPartOne),
+		String(resultPartTwo)
 	];
 }
 
 export {
-	readDigitsFromLine,
+	readPureDigitsFromLine,
+	readAllDigitsFromLine,
 	calculateResultSum,
 	solve
 }
