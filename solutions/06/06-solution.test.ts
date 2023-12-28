6c9d62a16af444712f349680777bf96db52d2fab
Antonin Vogt <antoninvogt@users.noreply.github.com>
2023-12-28 16:23:32 +0100

    add solution for day 06, part 1

---
+++ solutions/06/06-solution.test.ts
@@ -0,0 +1,62 @@
+import {
+	parseInput,
+	determineRecordInputNumbers,
+	determineNumberOfWinningInputs
+} from "./06-solution";
+
+describe("Day 06", () => {
+	describe("parseInput", () => {
+		it("should parse the example input", () => {
+			const input = [
+				"Time:      7  15   30",
+				"Distance:  9  40  200",
+				""
+			];
+
+			const result = parseInput(input);
+
+			expect(result).toEqual([
+				{ time: 7, record: 9 },
+				{ time: 15, record: 40 },
+				{ time: 30, record: 200 }
+			]);
+		});
+	});
+
+	describe("determineRecordInputNumbers", () => {
+		it("should find the possible inputs for the record time for the first puzzle example", () => {
+			const input = { time: 7, record: 9 };
+
+			const result = determineRecordInputNumbers(input);
+
+			expect(result[0]).toBeCloseTo(1.7, 1);
+			expect(result[1]).toBeCloseTo(5.3, 1);
+		});
+	});
+
+	describe("determineNumberOfWinningInputs", () => {
+		it("should find the number of ways to win puzzle example race no. 1", () => {
+			const input = { time: 7, record: 9 };
+
+			const result = determineNumberOfWinningInputs(input);
+
+			expect(result).toEqual(4);
+		});
+
+		it("should find the number of ways to win puzzle example race no. 1", () => {
+			const input = { time: 15, record: 40 };
+
+			const result = determineNumberOfWinningInputs(input);
+
+			expect(result).toEqual(8);
+		});
+
+		it("should find the number of ways to win puzzle example race no. 3", () => {
+			const input = { time: 30, record: 200 };
+
+			const result = determineNumberOfWinningInputs(input);
+
+			expect(result).toEqual(9);
+		});
+	});
+});
---
+++ solutions/06/06-solution.ts
@@ -0,0 +1,65 @@
+import * as inputHelper from "../../helpers/input";
+
+type Race = { time: number, record: number }
+
+function parseInput(inputLines: string[]): Race[] {
+	const raceTimesLine = inputLines[0];
+	const recordsLine = inputLines[1];
+
+	const raceTimes = raceTimesLine
+		.replace(/\s+/g, " ")
+		.split(" ")
+		.slice(1)
+		.map(Number);
+	const recordTimes = recordsLine
+		.replace(/\s+/g, " ")
+		.split(" ")
+		.slice(1)
+		.map(Number);
+
+	return raceTimes.map((raceTime, index) => {
+		return { time: raceTime, record: recordTimes[index] } as Race;
+	});
+}
+
+function determineRecordInputNumbers(race: Race): [ number, number ] {
+	// D = (time - b) * (t * c), where D is distance, time is race time, b is button push time, and c is speed per time interval the button is pushed (so, 1)
+	// After some moving terms around, comes to
+	// 0 = b² - time * b + D
+	// which, after quadratic formula, is..
+
+	const { time, record } = race;
+	const lowerInput = (time - Math.sqrt((-time) ** 2 - 4 * 1 * record)) / 2;
+	const higherInput = (time + Math.sqrt((-time) ** 2 - 4 * 1 * record)) / 2;
+
+	return [ lowerInput, higherInput ];
+}
+
+function determineNumberOfWinningInputs(race: Race): number {
+	const [ lowRecordInput, highRecordInput ] = determineRecordInputNumbers(race);
+
+	const lowestWinningInput = (lowRecordInput % 1 === 0) ? lowRecordInput + 1 : Math.ceil(lowRecordInput);
+	const highestWinningInput = (highRecordInput % 1 === 0) ? highRecordInput - 1 : Math.floor(highRecordInput);
+
+	return highestWinningInput - lowestWinningInput + 1;
+}
+
+async function solve(): Promise<string[]> {
+	const puzzleInput = await inputHelper.readPuzzleInput(__dirname, "./06-input.txt");
+
+	const races = parseInput(puzzleInput);
+	const winningInputs = races.map(determineNumberOfWinningInputs);
+
+	const result = winningInputs.reduce((total, next) => total * next, 1);
+
+	return [
+		String(result)
+	];
+}
+
+export {
+	parseInput,
+	determineRecordInputNumbers,
+	determineNumberOfWinningInputs,
+	solve
+}
